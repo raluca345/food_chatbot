46d4dcbad7e6882c9dd594407001f6e8f235720b
raluca345 <raluca345stamate@gmail.com>
2025-10-11 17:06:54 +0300

    improved the UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,22 +5,38 @@ function App() {
   const [activeTab, setActiveTab] = useState("food-image-generator");
 
   const handleTabChange = (tab) => {
-    console.log(tab);
     setActiveTab(tab);
   };
 
   return (
     <>
-      <div>
-        <button onClick={() => handleTabChange("food-pic-generator")}>
-          Generate Food Picture
+      <div className="App">
+        <button
+          className={activeTab === "food-image-generator" ? "active" : ""}
+          onClick={() => handleTabChange("food-image-generator")}
+        >
+          Generate Food Image
         </button>
-        <button onClick={() => handleTabChange("food-chat")}>
+        <button
+          className={activeTab === "food-chat" ? "active" : ""}
+          onClick={() => handleTabChange("food-chat")}
+        >
           Talk About Food
         </button>
-        <button onClick={() => handleTabChange("recipe-generator")}>
+        <button
+          className={activeTab === "recipe-generator" ? "active" : ""}
+          onClick={() => handleTabChange("recipe-generator")}
+        >
           Generate a Recipe
         </button>
+
+        <div>
+          {activeTab === "food-image-generator" && (
+            <h2>Food Image Generator</h2>
+          )}
+          {activeTab === "food-chat" && <h2>Hello, User</h2>}
+          {activeTab === "recipe-generator" && <h2>Recipe Generator</h2>}
+        </div>
       </div>
     </>
   );
